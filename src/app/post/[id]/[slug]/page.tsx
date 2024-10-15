152020505a3100615795cb11400c9255a78c24c2
taehwan01 <monte6198@gmail.com>
2024-10-16 02:57:33 +0900

    feat: 게시글 좋아요 추가

--- src/app/post/[id]/[slug]/page.tsx
+++ src/app/post/[id]/[slug]/page.tsx
@@ -12,13 +12,14 @@ const Markdown = dynamic(() => import('@uiw/react-md-editor').then((mod) => mod.
 export default function PostDetail() {
     const { id, slug } = useParams(); // URL에서 id와 slug 추출
     const [post, setPost] = useState<any | null>(null); // Post 타입을 정의하지 않은 경우, any로 처리
+    const [likeCounts, setLikeCounts] = useState<number>(0); // 좋아요 수 상태
+    const [hasLiked, setHasLiked] = useState<boolean>(false); // 좋아요 여부 상태
 
     useEffect(() => {
         const fetchPost = async () => {
             if (id) {
                 try {
                     const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts/${id}`);
-                    console.log(response);
                     setPost(response.data); // API 호출하여 id로 데이터 가져오기
                 } catch (error) {
                     console.error('Failed to fetch post:', error);
@@ -26,8 +27,53 @@ export default function PostDetail() {
             }
         };
 
+        const checkLikeStatus = async () => {
+            if (id) {
+                try {
+                    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts/${id}/like-status`);
+                    setHasLiked(response.data.hasLiked); // 좋아요 여부 설정
+                } catch (error) {
+                    console.error('Failed to check like status:', error);
+                }
+            }
+        };
+
+        const fetchLikeCounts = async () => {
+            if (id) {
+                try {
+                    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts/${id}/like-count`);
+                    setLikeCounts(response.data.likeCounts); // API 호출하여 좋아요 수 가져오기
+                } catch (error) {
+                    console.error('Failed to fetch likes:', error);
+                }
+            }
+        };
+
         fetchPost();
+        checkLikeStatus();
+        fetchLikeCounts();
     }, [id]);
+
+    // 좋아요 버튼 클릭 핸들러
+    const handleLikeClick = async () => {
+        try {
+            if (!hasLiked) {
+                setLikeCounts((prevLikes) => prevLikes + 1); // 좋아요 수 증가
+                setHasLiked(true); // 좋아요 상태 업데이트
+                await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/posts/${id}/like`, {}, { withCredentials: true });
+            } else {
+                setLikeCounts((prevLikes) => prevLikes - 1); // 좋아요 수 감소
+                setHasLiked(false); // 좋아요 상태 업데이트
+                await axios.post(
+                    `${process.env.NEXT_PUBLIC_API_URL}/posts/${id}/cancel-like`,
+                    {},
+                    { withCredentials: true }
+                );
+            }
+        } catch (error) {
+            console.error('Failed to update like status:', error);
+        }
+    };
 
     if (!post) {
         return <p>Loading...</p>;
@@ -41,8 +87,11 @@ export default function PostDetail() {
             <div data-color-mode='light'>
                 <Markdown source={post.content} className={styles.markdown} />
             </div>
-            {/* <Markdown source={post.content} className={styles.markdown} style={{ backgroundColor: 'orange' }} /> */}
             <p>{new Date(post.created_at).toLocaleDateString()}</p>
+            <div>
+                <button onClick={handleLikeClick}>{hasLiked ? '좋아요 취소' : '좋아요'}</button>
+                <span>{likeCounts}명이 좋아요를 눌렀습니다.</span>
+            </div>
         </div>
     );
 }
