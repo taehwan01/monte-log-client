de335b131f2b7792e7694d289e334ec16d885300
taehwan01 <monte6198@gmail.com>
2024-10-17 07:35:47 +0900

    style: 로그인 실패 페이지 수정

--- src/app/auth/login/failure/page.tsx
+++ src/app/auth/login/failure/page.tsx
@@ -1,10 +1,33 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import styles from './failure.module.css';
+import dittoImage from '../../../public/ditto.jpg';
+
 export default function LoginFailure() {
+    const [timer, setTimer] = useState(3);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimer((prev) => prev - 1);
+        }, 1000);
+
+        setTimeout(() => {
+            window.location.href = '/';
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div>
-            <h1>로그인 실패</h1>
-            <p>로그인이 거부되었습니다.</p>
+        <div id={styles.loginFailureContainer}>
+            <Image src={dittoImage} alt='ditto' id={styles.ditto} />
+            <div id={styles.loginFailureMessageContainer}>
+                <p>로그인이 거부되었습니다.</p>
+                <br />
+                <p>{timer > 0 ? `${timer}초 후 ` : ''}메인 페이지로 이동합니다.</p>
+            </div>
         </div>
     );
 }
